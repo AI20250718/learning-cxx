6576ee7de2a6e12402ef55b0feb58d37befe3e17
hangyuliu <liu_hang_yu@163.com>
2025-07-21 20:11:11 +0800

    feat:update

--- exercises/12_method_const/main.cpp
+++ exercises/12_method_const/main.cpp
@@ -5,7 +5,10 @@
 struct Fibonacci {
     int numbers[11];
     // TODO: 修改方法签名和实现，使测试通过
-    int get(int i) {
+    // int get(int i) {
+    // };
+    int get(int i) const {// Fibonacci::get 支持 constexpr 对象（如 FIB）
+        return numbers[i];
     }
 };
 
