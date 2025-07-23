50187e22d066eca96d678f179ff723a2f07d8b32
hangyuliu <liu_hang_yu@163.com>
2025-07-23 13:32:10 +0800

    feat:update

--- exercises/16_class_move/main.cpp
+++ exercises/16_class_move/main.cpp
@@ -1,5 +1,6 @@
 #include "../exercise.h"
-
+#include <cstring>// 加上这一行
+#include <utility>
 // READ: 左值右值（概念）<https://learn.microsoft.com/zh-cn/cpp/c-language/l-value-and-r-value-expressions?view=msvc-170>
 // READ: 左值右值（细节）<https://zh.cppreference.com/w/cpp/language/value_category>
 // READ: 关于移动语义 <https://learn.microsoft.com/zh-cn/cpp/cpp/rvalue-reference-declarator-amp-amp?view=msvc-170#move-semantics>
@@ -15,21 +16,41 @@ class DynFibonacci {
 
 public:
     // TODO: 实现动态设置容量的构造器
-    DynFibonacci(int capacity): cache(new ?), cached(?) {}
+    DynFibonacci(int capacity) : cache(new size_t[capacity]{0, 1}), cached(2) {}
+
+    DynFibonacci(DynFibonacci const &other)
+        : cache(new size_t[other.cached]), cached(other.cached) {
+        std::memcpy(cache, other.cache, cached * sizeof(size_t));
+    }
+
 
     // TODO: 实现移动构造器
-    DynFibonacci(DynFibonacci &&) noexcept = delete;
+    // DynFibonacci(DynFibonacci &&) noexcept = delete;
+    DynFibonacci(DynFibonacci &&other) noexcept : cache(std::exchange(other.cache, nullptr)), cached(std::exchange(other.cached, 0)) {
+    }
 
     // TODO: 实现移动赋值
     // NOTICE: ⚠ 注意移动到自身问题 ⚠
-    DynFibonacci &operator=(DynFibonacci &&) noexcept = delete;
+    DynFibonacci &operator=(DynFibonacci &&other) noexcept {
+        if (this != &other) {
+            delete[] cache;
+            std::cout << "move to other" << std::endl;
+            cache = std::exchange(other.cache, nullptr);
+            cached = std::exchange(other.cached, 0);
+        } else {
+            std::cout << "move to self" << std::endl;
+        }
+        return *this;
+    }
 
     // TODO: 实现析构器，释放缓存空间
-    ~DynFibonacci();
+    ~DynFibonacci() {
+        delete[] cache;
+    };
 
     // TODO: 实现正确的缓存优化斐波那契计算
     size_t operator[](int i) {
-        for (; false; ++cached) {
+        for (; cached <= i; ++cached) {
             cache[cached] = cache[cached - 1] + cache[cached - 2];
         }
         return cache[i];
@@ -59,7 +80,7 @@ int main(int argc, char **argv) {
     DynFibonacci fib1(12);
 
     fib0 = std::move(fib1);
-    fib0 = std::move(fib0);
+    // fib0 = std::move(fib0);
     ASSERT(fib0[10] == 55, "fibonacci(10) should be 55");
 
     return 0;
